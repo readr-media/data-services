2ce1eb685bcfc9464b8d9c59c1926bf8a0ec024b
hcchien <hcchien@gmail.com>
2023-10-24 16:52:07 +0800

    fix: load module error

--- server.py
+++ server.py
@@ -2,16 +2,16 @@ from flask import Flask, request
 from data_export import sheet2json, gql2json, upload_data
 from rss_generator import gql2rss
 from scheduled_update import status_update
-from election import president_factcheck
+from election import factcheck_data
 import os
 import json
 
 app = Flask(__name__)
 gql_endpoint = os.environ['GQL_ENDPOINT']
 
-@app.route("/president_factcheck2024")
+@app.route("/president_factcheck")
 def president_fackcheck_json():
-	president_factcheck()
+	factcheck_data()
 	return "ok"
 
 @app.route("/gql_to_json")
