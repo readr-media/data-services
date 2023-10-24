dadb31a6f7b8276133e8f2a04a20f629f00f2de2
hcchien <hcchien@gmail.com>
2023-10-24 16:43:38 +0800

    fix: load module error

--- election.py
+++ election.py
@@ -36,10 +36,10 @@ SELECT "Politic"."person", "Politic"."politicCategory", count(*) FROM "Politic",
 
 def president_factcheck():
 	categories = ['交通']
-	for category in categories:
-        gql_string = ''' query GetPresidents { personElections( orderBy:{ number: asc }, where: { election: {type: { equals: "總統" }, election_year_year: { equals: 2024 } }, mainCandidate: null }) { id number person_id { id name } politicsCount( where: { status: { equals: "verified" }, reviewed: { equals: true } }) politics( where: { status: { equals: "verified" }, reviewed: { equals: true } politicCategory: { name: { equals: '%s' } } }) { id content politicCategory { id name } positionChange { id isChanged factcheckPartner { id name } } positionChangeCount expertPoint { id expertPointSummary expert } expertPointCount factCheck { id factCheckSummary checkResultType factcheckPartner { id name } } factCheckCount repeat { id repeatSummary factcheckPartner { id name } } repeatCount } } }''' % (category)
-        data_endpoint = DATA_SERVICE + '/gql_to_json?bucket=' + WHORU_BUCKET + '&dest_file=files/json/president_' + category + '.json&gql_string=' + gql_string
-        r = requests.get(data_endpoint) 
+	#for category in categories:
+    #    gql_string = ''' query GetPresidents { personElections( orderBy:{ number: asc }, where: { election: {type: { equals: "總統" }, election_year_year: { equals: 2024 } }, mainCandidate: null }) { id number person_id { id name } politicsCount( where: { status: { equals: "verified" }, reviewed: { equals: true } }) politics( where: { status: { equals: "verified" }, reviewed: { equals: true } politicCategory: { name: { equals: '%s' } } }) { id content politicCategory { id name } positionChange { id isChanged factcheckPartner { id name } } positionChangeCount expertPoint { id expertPointSummary expert } expertPointCount factCheck { id factCheckSummary checkResultType factcheckPartner { id name } } factCheckCount repeat { id repeatSummary factcheckPartner { id name } } repeatCount } } }''' % (category)
+    #    data_endpoint = DATA_SERVICE + '/gql_to_json?bucket=' + WHORU_BUCKET + '&dest_file=files/json/president_' + category + '.json&gql_string=' + gql_string
+    #    r = requests.get(data_endpoint) 
     return "ok"
 
 if __name__=="__main__":
