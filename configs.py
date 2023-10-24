99175c03fc3d12e9f1349c6087a3e71305e33843
hcchien <hcchien@gmail.com>
2023-10-24 13:01:29 +0800

    feat: add president factcheck json

--- configs.py
+++ configs.py
@@ -5,3 +5,8 @@ feed_config = {
     }
 feed_config_check_list = ["baseURL", "title", "description", "link", "copyright", "image", "officialLine", "utmSource"]
 escapse_char = u'[^\u0020-\uD7FF\u0009\u000A\u000D\uE000-\uFFFD\U00010000-\U0010FFFF]+'
+homepage_data = {
+    "homepage_categories": 10,
+    "homepage_category_posts": 15,
+    "homepage_category_reports": 3
+}
--- server.py
+++ server.py
@@ -2,12 +2,17 @@ from flask import Flask, request
 from data_export import sheet2json, gql2json, upload_data
 from rss_generator import gql2rss
 from scheduled_update import status_update
+from election import president_factcheck
 import os
 import json
 
 app = Flask(__name__)
 gql_endpoint = os.environ['GQL_ENDPOINT']
 
+@app.route("/president_factcheck2024")
+def president_fackcheck_json():
+	president_factcheck()
+	return "ok"
 
 @app.route("/gql_to_json")
 def generate_json_from_gql():
