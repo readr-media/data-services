aadc501b1e616c5bcfa22a08adbafb18861816ab
hcchien <hcchien@gmail.com>
2023-10-24 15:29:02 +0800

    fix: the Docker file of the new wsgi

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from data_export import sheet2json, gql2json, upload_data
 from rss_generator import gql2rss
 from scheduled_update import status_update
-from election import president_factcheck
+#from election import president_factcheck
 import os
 import json
 
@@ -11,7 +11,7 @@ gql_endpoint = os.environ['GQL_ENDPOINT']
 
 @app.route("/president_factcheck2024")
 def president_fackcheck_json():
-	president_factcheck()
+	#president_factcheck()
 	return "ok"
 
 @app.route("/gql_to_json")
