c7c8fb39977ba889686c0412cec43a455f8d1d5a
Dylan Liu <gaespa007648@gmail.com>
2024-01-29 14:39:09 +0800

    fix: fix the loc url path error of sitemap index

--- sitemap.py
+++ sitemap.py
@@ -103,7 +103,7 @@ def generate_sitemap_index(sitemap_files):
     for info in sitemap_files:
         filename = info['filename']
         doc = ET.SubElement(root, "sitemap")
-        ET.SubElement(doc, 'loc').text = f'https://{BASE_URL}/{filename}'
+        ET.SubElement(doc, 'loc').text = f'https://{BASE_URL}{filename}'
         ET.SubElement(doc, 'lastmod').text = info['lastmod']
     xml_string = ET.tostring(root, encoding='utf-8')
     return xml_string
