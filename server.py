89a39c0a9c674a479d03930b6b744ba16be6deb6
Nick Chen <s27206383@gmail.com>
2024-07-22 16:24:15 +0800

    fix : modify the bug of TabError

--- server.py
+++ server.py
@@ -113,8 +113,9 @@ def sitemap_generator():
             gql_string = query.get_allPosts_string(publish_gt_time)
         else:
             gql_string = query.get_Posts_string(publish_gt_time)
+        
+        print(gql_string)
 	
-	print(gql_string)
         gql_result = query.gql_fetch(gql_endpoint=gql_endpoint, gql_string=gql_string)
         xml_strings = generate_news_sitemaps(
                 rows = gql_result['items'],
